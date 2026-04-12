a140d2bcb5867e6714c13886606d8734d16526ba
Shyamyemuka <ys304123@gmail.com>
2026-04-12 12:52:55 +0530

    Remove HF artifact directories from tracking

--- .hf_bundle_test/server/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Prompt Opt Env / PromptOptEnv environment server components."""
-
-from .prompt_opt_env_environment import PromptOptEnvEnvironment, PromptOptEnvironment
-
-__all__ = ["PromptOptEnvEnvironment", "PromptOptEnvironment"]
